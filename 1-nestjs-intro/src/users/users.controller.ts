19b2cedf25903086aa3cee4621b7736abf2d783f
Biraj Ghorasaine <birajghorasaine@cloudtechservice.com>
2025-09-08 14:16:05 +0545

    learned and applied pipe concept and use built-in pipe

--- 1-nestjs-intro/src/users/users.controller.ts
+++ 1-nestjs-intro/src/users/users.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Body,
   Query,
+  ParseIntPipe,
+  DefaultValuePipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 
@@ -22,16 +24,27 @@ export class UsersController {
   }
 
   //Get Users by querystring
-  @Get('query')
-  getAllUsersWithQuery(@Query() query: any) {
-    console.log(query);
+  // @Get('query')
+  // getAllUsersWithQuery(@Query() query: any) {
+  //   console.log(query);
+  //   return this.usersService.getAllUsers();
+  // }
+
+  @Get('limit')
+  getAllUsersWithLimit(
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(limit);
+    console.log(page);
     return this.usersService.getAllUsers();
   }
 
   // Get User By Id
   @Get(':id')
-  getUserById(@Param('id') id: string) {
-    return this.usersService.getUserById(Number(id));
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    console.log(typeof id);
+    return this.usersService.getUserById(id);
   }
 
   // Get user based on age and married status
@@ -78,8 +91,8 @@ export class UsersController {
 
   // Delete User
   @Delete('delete/:id')
-  deleteUser(@Param('id') id: string) {
-    return this.usersService.deleteUser(Number(id));
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.deleteUser(id);
   }
 }
 
@@ -94,4 +107,51 @@ export class UsersController {
 @Headers(name?: string)	req.headers / req.headers[name]
 @Ip()	req.ip
 @HostParam()	req.hosts
+*/
+
+/*
+Pipes: 
+A pipe is a class annotated with the @Injectable() decorator, which implements the 
+PipeTransform interface.
+
+Pipes have two typical use cases:
+
+transformation: transform input data to the desired form (e.g., from string to integer)
+validation: evaluate input data and if valid, simply pass it through unchanged; otherwise, 
+throw an exception
+
+In both cases, pipes operate on the arguments being processed by a controller route handler. 
+Nest interposes a pipe just before a method is invoked, and the pipe receives the arguments 
+destined for the method and operates on them. Any transformation or validation operation takes 
+place at that time, after which the route handler is invoked with any (potentially) 
+transformed arguments.
+
+
+@PipeTransform()
+export class ParseIntPipe implements PipeTransform {
+  transform(value: string) {
+    return parseInt(value);
+  }
+}
+
+Pipes run inside the exceptions zone. This means that when a Pipe throws an exception it is 
+handled by the exceptions layer 
+(global exceptions filter and any exceptions filters that are applied to the current context).
+
+
+Built-in pipes#
+Nest comes with several pipes available out-of-the-box:
+
+ValidationPipe
+ParseIntPipe
+ParseFloatPipe
+ParseBoolPipe
+ParseArrayPipe
+ParseUUIDPipe
+ParseEnumPipe
+DefaultValuePipe
+ParseFilePipe
+ParseDatePipe
+
+They're exported from the @nestjs/common package.
 */
