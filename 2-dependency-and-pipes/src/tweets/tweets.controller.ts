9c2f75d695b1837bbd2047c67fc8d4f70366ded0
Biraj Ghorasaine <birajghorasaine@cloudtechservice.com>
2025-09-16 19:53:21 +0545

    understand and implement inter modular dependency

--- 2-dependency-and-pipes/src/tweets/tweets.controller.ts
+++ 2-dependency-and-pipes/src/tweets/tweets.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Logger, ParseIntPipe } from '@nestjs/common';
 import { TweetsService } from './tweets.service';
 
 @Controller('tweets')
 export class TweetsController {
-  constructor(private tweetsService: TweetsService) {}
+  private readonly logger = new Logger(TweetsController.name);
+
+  constructor(private readonly tweetsService: TweetsService) {}
+
+  @Get()
+  getAllTweets() {
+    this.logger.log('Fetching tweets for all users');
+    return this.tweetsService.getTweets();
+  }
+
+  @Get(':userId')
+  getTweetsByUser(@Param('userId', ParseIntPipe) userId: number) {
+    this.logger.log(`Fetching tweets for user ID: ${userId}`);
+    return this.tweetsService.getTweets(userId);
+  }
 }
--- 2-dependency-and-pipes/src/tweets/tweets.service.ts
+++ 2-dependency-and-pipes/src/tweets/tweets.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class TweetsService {}
+export class TweetsService {
+  constructor(private readonly userService: UsersService) {}
+  private readonly tweets = [
+    { id: 1, text: 'Hello World', date: new Date(), userId: 1 },
+    { id: 2, text: 'My second tweet', date: new Date(), userId: 2 },
+    { id: 3, text: 'NestJS is awesome', date: new Date(), userId: 3 },
+    {
+      id: 4,
+      text: 'Dependency Injection is powerful',
+      date: new Date(),
+      userId: 4,
+    },
+    { id: 5, text: 'I love programming', date: new Date(), userId: 5 },
+    { id: 6, text: 'TypeScript is great', date: new Date(), userId: 6 },
+  ];
+  getTweets(userId?: number) {
+    const user = this.userService.getAllUsers();
+    if (userId) {
+      return this.tweets
+        .filter((tweet) => tweet.userId === userId)
+        .map((tweet) => ({
+          ...tweet,
+          user: user.find((u) => u.id === tweet.userId),
+        }));
+    }
+    return this.tweets.map((tweet) => ({
+      ...tweet,
+      user: user.find((u) => u.id === tweet.userId),
+    }));
+  }
+}
