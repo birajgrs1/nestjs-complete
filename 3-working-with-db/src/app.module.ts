e9e1944a4b6f976fd77bb2de350b75e020d895c6
Biraj Ghorasaine <birajghorasaine@cloudtechservice.com>
2025-09-18 13:29:40 +0545

    working with sychronous connection of working with postgres-nestjs

--- 3-working-with-db/src/app.module.ts
+++ 3-working-with-db/src/app.module.ts
@@ -5,9 +5,25 @@ import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { TweetsModule } from './tweets/tweets.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, TweetsModule, AuthModule],
+  imports: [
+    UsersModule,
+    TweetsModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'birajgrs@1',
+      database: 'nestjs',
+      autoLoadEntities: true,
+      synchronize: true,
+      entities: [],
+    }),
+  ],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
