341f46d5cf581caf1f10fdb33503b69f7890106c
Biraj Ghorasaine <birajghorasaine@cloudtechservice.com>
2025-09-24 16:14:51 +0545

    Creating a Profile with User

--- 3-working-with-db/src/users/users.module.ts
+++ 3-working-with-db/src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { User } from './users.entity';
+import { Profile } from 'src/profiles/profile.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Profile])],
   providers: [UsersService],
   exports: [UsersService],
 })
--- 3-working-with-db/src/users/users.service.ts
+++ 3-working-with-db/src/users/users.service.ts
@@ -1,14 +1,18 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Repository, DeepPartial } from 'typeorm';
 import { User } from './users.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { Profile } from 'src/profiles/profile.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+
+    @InjectRepository(Profile)
+    private profileRepository: Repository<Profile>,
   ) {}
 
   // Get all users
@@ -24,14 +28,31 @@ export class UsersService {
   }
 
   // Create a new user
-  async createUser(userDto: CreateUserDto): Promise<User> {
-    // Check if user already exists
-    const existingUser = await this.findByEmail(userDto.email);
-    if (existingUser) {
-      throw new ConflictException('User already exists');
-    }
+  // async createUser(userDto: CreateUserDto): Promise<User> {
+  //   // Check if user already exists
+  //   const existingUser = await this.findByEmail(userDto.email);
+  //   if (existingUser) {
+  //     throw new ConflictException('User already exists');
+  //   }
 
-    const newUser = this.userRepository.create(userDto as DeepPartial<User>);
-    return await this.userRepository.save(newUser);
+  //   const newUser = this.userRepository.create(userDto as DeepPartial<User>);
+  //   return await this.userRepository.save(newUser);
+  // }
+
+  public async createUser(userDto: CreateUserDto): Promise<User> {
+    // Create a profile and save
+    let profile = this.profileRepository.create(
+      userDto.profile as DeepPartial<Profile>,
+    );
+    profile = await this.profileRepository.save(profile);
+
+    // Create a user object
+    const user = this.userRepository.create(userDto as DeepPartial<User>);
+
+    // Set the profile
+    user.profile = profile;
+
+    // Save the user object and return the full user with the profile populated
+    return await this.userRepository.save(user);
   }
 }
